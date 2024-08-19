78457498ad7b16a1e7ce7b3692d86552ca3067a3
jheinifferoliveira <jheiniffer2013@gmail.com>
2024-08-19 12:26:18 -0300

    feat: Desenvolvimento dos testes de integração para os endpoints de usuário.

---
+++ UsuariosApp.Tests/Helpers/TestHelper.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UsuariosApp.Tests.Helpers
+{
+    public static class TestHelper
+    {
+        public static StringContent Serialize(object request)
+            => new StringContent
+            (JsonConvert.SerializeObject(request), Encoding.UTF8, "application/json");
+
+        public static HttpClient CreateClient()
+            => new WebApplicationFactory<Program>().CreateClient();
+
+
+    }
+}
